b5e631b9f50e48f5fd0ead0add99d7ce468abfd7
Liev Haroche <Liev Haroche@10.229.42.202>
2018-01-17 14:37:18 -0800

    More for the subsystems

--- src/org/usfirst/frc/team6814/robot/subsystems/RightMotor.java
+++ src/org/usfirst/frc/team6814/robot/subsystems/RightMotor.java
@@ -1,11 +1,16 @@
 package org.usfirst.frc.team6814.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class RightMotor extends Subsystem{
+	protected Spark motor = new Spark(1);
 	
 	public void initDefaultCommand() {
 		
+	}
+	
+	public void run(double speed) {
+		motor.set(speed);
 	}
 }
