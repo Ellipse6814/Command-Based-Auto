4bfd08833ebf187e97d7da5270194dba38f75158
Liev Haroche <Liev Haroche@10.229.42.202>
2018-01-17 13:36:10 -0800

    First Commit

--- src/org/usfirst/frc/team6814/robot/Robot.java
+++ src/org/usfirst/frc/team6814/robot/Robot.java
@@ -22,7 +22,7 @@ import org.usfirst.frc.team6814.robot.commands.Drive;
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
+ * creating this project, you must also update the build.properties h file in the
  * project.
  */
 public class Robot extends TimedRobot {
